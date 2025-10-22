e875fe5d9269bd6a44105b46a509de7292df3e71
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-10-22 09:41:06 +0300

    changed configuration file playwright.config.js and fixed import of the application in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@Hexlet/testing-task-manager'
+import App from '@hexlet/testing-task-manager'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
