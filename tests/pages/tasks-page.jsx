1f619babd09fe44f74d5ef5914e62257eedf30dd
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-11-09 17:18:53 +0300

    changed tests/pages/tasks-page.jsx

--- tests/pages/tasks-page.jsx
+++ tests/pages/tasks-page.jsx
@@ -13,11 +13,11 @@ export class TasksPage {
 
   async createTask(assignee, title, content, statusOption, label) {
     await this.page.click(`[aria-label="Create"]`)
-    await this.page.selectOption(`div[aria-labelledby=":r39:-label :r39:"]`, assignee)
+    await this.page.selectOption(`div[role="combobox"]:nth-child(1)`, assignee)
     await this.page.fill(`input[name="title"]`, title)
     await this.page.fill(`textarea[name="content"]`, content)
-    await this.page.selectOption('div[aria-labelledby=":r3n:-label :r3n:"]', statusOption)
-    await this.page.selectOption(`div[aria-labelledby=":r3t:-outlined-label :r3t:"]`, label)
+    await this.page.selectOption('div[role="combobox"]:nth-child(2)"', statusOption)
+    await this.page.selectOption(`div[role="combobox"]:nth-child(3)`, label)
     await this.page.click(`[aria-label="Save"]`)
   }
 
